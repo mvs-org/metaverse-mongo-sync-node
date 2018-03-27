63295a90500d6163f3f1c605a4be91dc7b189734
Sven <sven@mutzl.org>
2018-03-27 12:50:47 +0200

    Removed dup blockhash

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ function syncBlock(number) {
                             return Promise.all(block.txs.transactions.map((tx) => {
                                     header.txs.push(tx.hash);
                                     tx.height = number;
-                                    tx.blockhash = header.hash;
                                     tx.orphan = 0;
                                     tx.block = header.hash;
                                     return MongoDB.addTx(tx).catch(() => {});
