dbcb309ecd92df69f70dde6fc9a5d3d885735034
Sven <sven@mutzl.org>
2018-03-30 13:59:18 +0200

    Initialize pools

--- models/Mongo.js
+++ models/Mongo.js
@@ -49,6 +49,82 @@ function init() {
                     height: -1
                 }),
                 database.createCollection('asset'),
+                database.collection('pool').createIndex({
+                    name: 1
+                }, {
+                    unique: true
+                }),
+                function() {
+                    try {
+                        database.collection('pool').insert([{
+                                name: "uupool",
+                                url: 'http://www.uupool.cn',
+                                origin: 'China',
+                                addresses: ['M97VaaTYnKtGzfMFrreitiYoy84Eueb16N']
+                            },
+                            {
+                                name: "xinyuanjie",
+                                url: 'http://xinyuanjie.org',
+                                origin: 'China',
+                                addresses: ['MKD5pmeyR14KGUxLo7YEECbgJ7MfrLPdTG']
+                            },
+                            {
+                                name: "dodopool",
+                                url: 'http://etp.dodopool.com',
+                                origin: 'UK',
+                                addresses: ['M8vkrEVPJCDn54L3TN64W3Grrq3KkBNVXh']
+                            },
+                            {
+                                name: "huopool",
+                                url: 'http://cryptopoolpond.com/#/',
+                                origin: 'China',
+                                addresses: ['MGr3pfTK8qJx3JEVnkTBjUSQZVK4jDujpZ']
+                            },
+                            {
+                                name: "metaverse.farm",
+                                url: 'https://metaverse.farm',
+                                origin: 'UK',
+                                addresses: ['MEVgP8kvucyR9523t71FVKSiZccQjeK4ki']
+                            },
+                            {
+                                name: "altpool.pro",
+                                url: 'http://etp.altpool.pro',
+                                origin: 'Europe',
+                                addresses: ['M97sAWjC2Du6RNBaHiCoqZFAGYXWrTM9At']
+                            },
+                            {
+                                name: "fairpool",
+                                url: 'https://etp.fairpool.xyz',
+                                origin: 'US',
+                                addresses: ['MGodWFRV7wu3jzwqfDakSCoH6ouhBL2TVG']
+                            },
+                            {
+                                name: "comining.io",
+                                url: 'http://comining.io',
+                                origin: 'Russia',
+                                addresses: ['MNd7ZeeadbSzEAtXmQ7B1SsoEoi7kHUWPg']
+                            },
+                            {
+                                name: "mvs",
+                                url: 'http://pool.mvs.live',
+                                origin: 'China',
+                                addresses: ['MFuFkdbp77YFQ24CnfLdLaKCdh9ASsR4r2']
+                            },
+                            {
+                                name: "cryptopoolpond",
+                                url: 'http://cryptopoolpond.com/#/',
+                                origin: 'US/Europe',
+                                addresses: ['MPVYH9GQGZkJc4rZ4ZEx9bnV1mpa3M4whw']
+                            },
+                            {
+                                name: "sandpool.org",
+                                url: 'http:/etp.sandpool.org',
+                                origin: 'Europe',
+                                addresses: ['MWHLJTawEecdiz8xBK98GB6MGD1qzYRmrP']
+                            }
+                        ]);
+                    } catch (e) {}
+                }
             ])
         );
 }
@@ -217,8 +293,8 @@ function getAsset(asset) {
 function connect(url, name) {
     return new Promise((resolve, reject) => {
         MongoClient.connect(url, {
-          w: 1,
-          j: false
+            w: 1,
+            j: false
         }, (err, con) => {
             if (err) throw err;
             else {
